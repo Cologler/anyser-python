ee220bbffe42668ba5dff80735fc9328410130ef
Cologler <skyoflw@gmail.com>
2020-03-23 19:07:08 +0800

    test format not found

--- tests/test_anyser.py
+++ tests/test_anyser.py
@@ -5,7 +5,22 @@
 #
 # ----------
 
+import pytest
+
+from anyser import *
+from anyser.err import FormatNotFoundError
+
 def test_imported_std():
     from anyser.core import _REGISTERED_SERIALIZERS
 
     assert 'json' in _REGISTERED_SERIALIZERS
+
+def test_use_unknown_format():
+    with pytest.raises(FormatNotFoundError):
+        loadb(b'', 'ssssss')
+    with pytest.raises(FormatNotFoundError):
+        dumpb(b'', 'ssssss')
+    with pytest.raises(FormatNotFoundError):
+        loads('', 'ssssss')
+    with pytest.raises(FormatNotFoundError):
+        dumps('', 'ssssss')
